8f70d63d315e8592e862ac5bd1d17c1cf07e4fd8
Nick <njd5059@gmail.com>
2019-02-11 19:51:21 -0500

    d
    d

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
-exports.twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>',
-  }
+console.log('this is loaded');
+
+	var twitterKeys = {
+		consumer_key: 'BroaPgoM1daTNer69MK1RwZDg',
+		consumer_secret: '0xh2c05VzmTKcnW9GqHStHg7W2eoKTL6d7hxN98rNl5dwM5ACu',
+		access_token_key: '	862385844-g2yy78Rs535f0OedtLExeUwHE4e4jEOOQziAiUQM',
+		access_token_secret: 'LRiNij6LqVjsHCt6dqgGI1UmOx1pIGAWo8EogDOEHZ42B',
+	}
+
+	module.exports = twitterKeys;
+
